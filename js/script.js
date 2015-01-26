41ec33d5057cfb23e7ecff2bfad719470be5a130
Anton Gilgur <agilgur5@gmail.com>
2015-01-25 21:48:34 -0500

    framework ready, just need to finish adding the 'bloggy' things

---
+++ js/script.js
@@ -0,0 +1,476 @@
+var json = [
+	{
+		title: "Meeting Werner Zorman",
+		date: "10/23/2013",
+		photo_url: "./img/werner_zorman.jpg",
+		caption: "Werner Zorman",
+		body: "Freshman year. After just figuring out which college I'm going to, I already have to start thinking about careers.",
+		read_more_id: "meeting_werner_zorman",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Engineering Leadership Certification Program Info Session",
+		date: "11/05/2013",
+		photo_url: "./img/olin_hall.jpg",
+		caption: "Olin Hall",
+		body: "I decided to go to the information session and to find out some more about the leadership program. Apparently, I came to the very last info session and there was only one other person other than Werner there, Daniel Galvez, who would later become an awesome friend.",
+		read_more_id: "leadership_program_info_session",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Engineering Leadership Certification Program Application",
+		date: "11/12/2013",
+		photo_url: "./img/qualtrics.png",
+		caption: "Qualtrics - where the application was hosted",
+		body: "After a bit of consideration, I thought it wouldn't hurt to apply, if I ended up not wanting to take it, I could always say no after being accepted.",
+		read_more_id: "leadership_program_application",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Engineering Leadership Certification Program Acceptance",
+		date: "11/25/2013",
+		photo_url: "./img/acceptance.png",
+		caption: "I got accepted!",
+		body: "Right before Thanksgiving break, I get an awesome email... I got accepted into the leadership program!",
+		read_more_id: "leadership_program_acceptance",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "First Retreat",
+		date: "1/24/2014",
+		photo_url: "./img/retreat_circle.jpg",
+		caption: "Light on the Hill Retreat Center",
+		body: "First class... yep, just your typical introductory session with lots of course information... except not really.",
+		read_more_id: "first_retreat",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Storytime",
+		date: "1/25/2014",
+		photo_url: "./img/candle.jpg",
+		caption: "A candle-lit, emotionally intensive story telling session",
+		body: "On the second night of the retreat, Erica and Werner leave us with our TAs and some candles to give us a chance to talk about our own stories. Little did anyone expect it to turn into something amazing.",
+		read_more_id: "storytime",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Typing",
+		date: "1/26/2014",
+		photo_url: "./img/typing.jpg",
+		caption: "I got typed as Sage!",
+		body: "On the day we're going to leave, it's finally my turn to go up and be \"typed\".",
+		read_more_id: "typing",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Last Day of First Retreat",
+		date: "1/26/2014",
+		photo_url: "./img/first_retreat.jpg",
+		caption: "These awesome people",
+		body: "This is more of a photo holder :)"
+	},
+	{
+		title: "Values",
+		date: "1/31/2014",
+		photo_url: "./img/values.jpg",
+		caption: "My values worksheet",
+		body: "It's time to identify what my values are and why they're important to me.",
+		read_more_id: "values",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Inner Critic vs. Inner Champion",
+		date: "2/07/2014",
+		photo_url: "./img/luffy.jpg",
+		caption: "Luffy - who I drew as my Inner Champion",
+		body: "The Inner Critic and the Inner Champion, the two forces always fighting within everyone's minds.",
+		read_more_id: "inner_critic",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Resilience",
+		date: "2/14/2014",
+		photo_url: "./img/risa_mish.jpg",
+		caption: "Risa Mish - the presenter for the resilience topic",
+		body: "How do I keep myself healthy? Why do I get up in the morning? How do I continue every day?",
+		read_more_id: "resilience",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Leading from Strengths",
+		date: "2/21/2014",
+		photo_url: "./img/strengths.jpg",
+		caption: "Some of my strengths revealed in StrengthsQuest",
+		body: "StrengthsQuest, Reflected Best Self, and more. Who am I at my best?",
+		read_more_id: "strengths",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Purpose",
+		date: "2/28/2014",
+		photo_url: "./img/hungers.jpg",
+		caption: "Some of my hungers revealed in the exercise",
+		body: "What is my purpose? Simple question, right? Not really.",
+		read_more_id: "purpose",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Listening",
+		date: "3/07/2014",
+		photo_url: "./img/listening.jpg",
+		caption: "What real listening looks like",
+		body: "So you know how well you listen can sometimes change? Well apparently you can define three levels of listening.",
+		read_more_id: "listening",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Presentation Skills",
+		date: "3/14/2014",
+		photo_url: "./img/presenting.png",
+		caption: "Let's replace PowerPoint with something more \"sticky\"",
+		body: "How do you get the attention of a group of people?",
+		read_more_id: "presenting",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Project Idea Presentations",
+		date: "3/21/2014",
+		photo_url: "./img/presentations.png",
+		caption: "Me pitching my idea, wearing one of my favorite outfits",
+		body: "28 people. 2 hours. About 1 minute each. I wonder what everyone's going to pitch.",
+		read_more_id: "presentations",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Second Retreat",
+		date: "3/22/2014",
+		photo_url: "./img/sorting.jpg",
+		caption: "Yes, there was a sorting hat",
+		body: "Well, we finished pitching, we voted which ones were our top choices, and we know our purposes... What team will I be in?",
+		read_more_id: "sorting",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Effective Teams",
+		date: "3/22/2014",
+		photo_url: "./img/second_retreat.jpg",
+		caption: "Toxic Waste Disposal?",
+		body: "Now that pitches are completed and teams are formed, it's time to make sure we can be effective teams. Luckily, I'm part of Productivity & *Efficiency*.",
+		read_more_id: "effective_teams",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Goals and Accountability",
+		date: "4/11/2014",
+		photo_url: "./img/patrick_thean.jpg",
+		caption: "Guest speaker Patrick Thean",
+		body: "In 2 days, I got to speak with Patrick Thean at least 3 times. It was exciting to learn and receive insight from someone with so much experience.",
+		read_more_id: "goals_and_accountability",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Project Management",
+		date: "4/25/2014",
+		photo_url: "./img/swot_analysis.png",
+		caption: "SWOT Analysis",
+		body: "We figured out our projects, now let's figure out how to manage them properly so we have some great results to show by the end.",
+		read_more_id: "project management",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Survey",
+		date: "5/20/2014",
+		photo_url: "./img/survey.png",
+		caption: "The survey results we got to determine interest in our product",
+		body: "We sent out a survey across many list-servs and Facebook groups. The response... positive!",
+		read_more_id: "survey",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Summer Communication",
+		date: "6/15/2014",
+		photo_url: "./img/slack.png",
+		caption: "Slack - probably the best tool our group used",
+		body: "How do we communicate and be productive when we're all in different places doing different things?",
+		read_more_id: "summer_communication",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "App Design",
+		date: "7/15/2014",
+		photo_url: "./img/app_design.png",
+		caption: "Some of the paper prototypes I drew over the summer",
+		body: "Let's make an effective user experience. But first... we need to define user experience.",
+		read_more_id: "app_design",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Third Retreat",
+		date: "9/02/2014",
+		photo_url: "./img/scavenger_hunt.jpg",
+		caption: "Doing some volunteering by pushing shopping carts out of the parking lot at Target",
+		body: "A new semester with the same great people. First thing we do... a scavenger hunt?",
+		read_more_id: "third_retreat",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Brainstorming",
+		date: "9/15/2014",
+		photo_url: "./img/new_room.jpg",
+		caption: "The new room where the second semester will be conducted",
+		body: "How do we brainstorm effectively? Also, who are all these new people?",
+		read_more_id: "brainstorming",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Managing Conflict",
+		date: "9/29/2014",
+		photo_url: "./img/conflict.jpg",
+		caption: "Survey results to determine if our team's on the same page",
+		body: "How do we deal with conflict? And how to make sure we deal with it well.",
+		read_more_id: "conflict",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Ethics",
+		date: "10/06/2014",
+		photo_url: "./img/ethics.jpg",
+		caption: "Okay, I couldn't find a picture of Park",
+		body: "What happens when people have to make tough choices... do they violate ethical and environmental choices? Or follow their own ethical standards?",
+		read_more_id: "ethics",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Decision Making for Leaders",
+		date: "10/20/2014",
+		photo_url: "./img/decision_making.jpg",
+		caption: "Some of my results from the decision making survey. Also, my name is apparently synonymous with John Doe.",
+		body: "I guess I don't make perfect decisions either. And there's research to explain why too!",
+		read_more_id: "decision_making",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Feedback",
+		date: "10/27/2014",
+		photo_url: "./img/feedback.jpg",
+		caption: "Feedback in a simple diagram",
+		body: "Okay, well it's not really that simple. Today we learned how to give and receive effective feedback.",
+		read_more_id: "feedback",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Power and Influence",
+		date: "11/03/2014",
+		photo_url: "./img/influence.jpg",
+		caption: "Some of my scores on the influence survey",
+		body: "What is power and how do we influence people? Let's see with a Kidney transplant case study.",
+		read_more_id: "power_influence",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Networking",
+		date: "11/10/2014",
+		photo_url: "./img/networking.jpg",
+		caption: "A drawing of part of my network",
+		body: "Networks aren't all that simple, but they can be incredibly useful in many ways.",
+		read_more_id: "networking",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Building Culture",
+		date: "11/17/2014",
+		photo_url: "./img/cultural_web.png",
+		caption: "The Cultural Web",
+		body: "No, but seriously, how do you build a culture? What is culture even?",
+		read_more_id: "culture",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Final Presentations",
+		date: "11/31/2014",
+		photo_url: "./img/final_presentations.jpg",
+		caption: "The Goalden Hour Team",
+		body: "Time to wrap everything up. What did we do the past 6 months?",
+		read_more_id: "ethics",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Last Class",
+		date: "11/31/2014",
+		photo_url: "./img/last_class.jpg",
+		caption: "I love these guys",
+		body: "Many tears were shed that day... across another candle-lit circle",
+		read_more_id: "last_class",
+		read_more_html: "blah blah balh"
+	},
+	{
+		title: "Final Notes",
+		date: "1/25/2015",
+		photo_url: "./img/final_notes.png",
+		caption: "Inception?",
+		body: "What happens next? How will I be courageous and develop myself further?",
+		read_more_id: "final_notes",
+		read_more_html: "Other than the leadership program ending, quite a bit of events occurred at the end of last semester that may require me to fundamentally change many things in my life. I'm still kind of figuring this out, so I'm not entirely sure what this will entail. But what I do know is that it will require a lot of courage. <br /> <br /> To me, courage is the ability to step out of my comfort zone. To do things I might not do on an every day basis or that may even be a bit unnatural. It's the willingness to take risks, try new things, and do things differently. Some of the biggest places in my life where I have to be courageous is with friends. I've always had a tough time making connections, and though I've always known the importance of friendship and always highly valued my friends, I've only recently come to realize just how necessary they are. I need to be courageous to be able to rely on others, to open up to others and really say how I feel. The leadership program, the people in it, and the events that transpired at the same time have taught me so many things that have really been helpful in the past months. I can't describe enough how appreciative I am of these things and how they've helped me move forward and make next steps. <br /> <br /> I also need to be courageous so that I don't fear entirely new places and people. Where my career and aspirations take me, nobody really knows, but I have to be comfortable going wherever life takes me, while at the same time knowing myself and my personal limits."
+	}
+];
+
+$(function() {
+
+	var direction = 'newest';
+
+	setupTimeline(json);
+
+	/**
+	 * Get the timestamp (milliseconds) given the year.
+	 * If beginning is true, timestamp is 1/1/year 12:00:00:000
+	 * If beginning is false, timestamp is 12/31/year 23:59:59:999
+	 */
+	function getTimestamp(year, beginning){
+		if (beginning)
+			return Date.parse('January 1, ' + year);
+		else
+			return Date.parse('December 31, ' + year) + 86400000 - 1 ; // plus 1 day, minus 1 millisecond
+	}
+
+	/**
+	 * Load the data into Isotope
+	 */
+	function setupTimeline(data){
+		// load the Handlebar templates into memory
+		var source = $('#post-template').html();
+		var postTemplate  = Handlebars.compile(source);
+		source = $('#year-marker-template').html();
+		var yearMarkerTemplate  = Handlebars.compile(source);
+
+		var years = [];
+		$.each(data, function(i, val){
+			// save the years so we can create year markers
+			val.timestamp = Date.parse(val.date);
+			var year = new Date(val.timestamp).getFullYear();
+			if (years.indexOf(year) < 0)
+				years[years.length] = year;
+
+			// combine data & templqate
+			var html = postTemplate(val);
+			$('#timeline').append(html);
+		});
+
+		// add a year marker for each year that has a post
+		$.each(years, function(i, val){
+			var timestamp;
+			if (direction == 'newest')
+				timestamp = getTimestamp(val, false);
+			else
+				timestamp = getTimestamp(val, true);
+			var context = {year: val, timestamp: timestamp};
+			var html = yearMarkerTemplate(context);
+			$('#timeline').append(html);
+		});
+
+		$('#timeline').imagesLoaded(function(){
+			$('#timeline').isotope({
+				itemSelector : '.item',
+				transformsEnabled: true,
+				layoutMode: 'spineAlign',
+				spineAlign:{
+					gutterWidth: 56
+				},
+				getSortData: {
+					timestamp: function($elem){
+						return parseFloat($elem.find('.timestamp').text());
+					}
+				},
+				sortBy: 'timestamp',
+				sortAscending: false,
+				itemPositionDataEnabled: true
+			});
+		});
+
+		// add open/close buttons to each post
+		$('#timeline .item.post').each(function(){
+			$(this).find('.inner').append('<a href="#" class="open-close"></a>');
+		});
+
+		$('#timeline .item a.open-close').click(function(e){
+			$(this).siblings('.body').slideToggle(function(){
+				$('#timeline').isotope('reLayout');
+			});
+			$(this).parents('.post').toggleClass('closed');
+			$('#expand-collapse-buttons a').removeClass('active');
+			e.preventDefault();
+		});
+
+
+		$('#buttons a.expand-all').click(function(e){
+			$('.post .body').slideDown(function(){
+				$('#timeline').isotope('reLayout');
+			});
+			$('.post').removeClass('closed');
+			$('#expand-collapse-buttons a').removeClass('active');
+			$(this).addClass('active');
+			e.preventDefault();
+		});
+
+		$('#buttons a.collapse-all').click(function(e){
+			$('.post .body').slideUp(function(){
+				$('#timeline').isotope('reLayout');
+			});
+			$('.post').addClass('closed');
+			$('#expand-collapse-buttons a').removeClass('active');
+			$(this).addClass('active');
+			e.preventDefault();
+		});
+
+	}
+
+	/*
+	 * Set the timestamp of the year markers to either the beginning or end of
+	 * the year
+	 */
+	function updateYearMarkers(beginning){
+		$('.year-marker').each(function(){
+			var $this = $(this);
+			var year = parseInt($this.find('.year').text());
+			var timestamp = getTimestamp(year, beginning);
+			$this.find('.timestamp').text(timestamp);
+		});
+	}
+
+	/*
+	 * Keep the actual line from extending beyond the last item's date tab
+	 */
+	function adjustLine(){
+		var $lastItem = $('.item.last');
+		var itemPosition = $lastItem.data('isotope-item-position');
+		var dateHeight = $lastItem.find('.date').height();
+		var dateOffset = $lastItem.find('.date').position();
+		var innerMargin = parseInt($lastItem.find('.inner').css('marginTop'));
+		var lineHeight = itemPosition.y + innerMargin + dateOffset.top + (dateHeight / 2);
+		$('#line').height(lineHeight);
+	}
+
+	$('#sort-buttons a').click(function(e){
+		var $this = $(this);
+		// don't proceed if already selected
+		if ($this.hasClass('active')) {
+			return false;
+		}
+
+		$('#sort-buttons a').removeClass('active');
+		$this.addClass('active');
+		var $yearMarkers = $('.year-markers');
+		if ($this.hasClass('sort-newest')){
+			updateYearMarkers(false);
+			$('#timeline').isotope('reloadItems').isotope({sortAscending: false});
+		}
+		else{
+			updateYearMarkers(true);
+			$('#timeline').isotope('reloadItems').isotope({sortAscending: true});
+		}
+		e.preventDefault();
+	});
+
+	$('#timeline').resize(function(){ // uses "jQuery resize event" plugin
+		adjustLine();
+	});
+});
